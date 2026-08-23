d6c98db61aba631d597f68259df902a7a2d0ec64
Thaylane da Silva <silvathaylane59@gmail.com>
2026-08-23 00:08:03 -0300

    Tempo de resposta

--- main.go
+++ main.go
@@ -7,45 +7,11 @@ import (
 		"net"
 	"time")
 
-<<<<<<< HEAD
-func gerenciarConexaoNaoBloqueante (conexao net.Conn) {
-	defer conexao.Close() 
-
-	for{
-		//deadline para ler
-		conexao.SetReadDeadline(time.Now().Add(time.Second))
-		// Ler do clinete 
-		buf := make ([]byte, 1024)
-		n,err := conexao.Read(buf) //bloqueia ate retornar o numero de bytes lidos 
-		
-		if err != nil {
-			if netErr, ok := err.(net.Error); ok && netErr.Timeout(){
-				continue
-			} else {
-				fmt.Println("Conexao Fechada: ", err)
-				break
-			}
-		}
-		//sucesso
-		fmt.Println("Recebida: ", string (buf[:n]))
-
-		//deadline
-		conexao.SetWriteDeadline(time.Now().Add(time.Second))
-
-		//mesnagem para o cliente 
-		fmt.Fprintln(conexao, "Ouvi!")
-
-		if err != nil{
-			log.Println("Erro para escrever para cliente", err)
-			break
-		}
-=======
-func gerenciarConexaoBloqueante (conexao net.Conn){
+func gerenciarConexaoNaoBloqueante (conexao net.Conn){
 	defer conexao.Close()
 
 	for{
 		conexao.SetDeadline(time.Now().Add(time.Second))
->>>>>>> ec3f6876febe96089581d04c037b52351b173d89
 
 		buf:= make([]byte, 1024)
 		n, err := conexao.Read(buf)
@@ -68,10 +34,6 @@ func gerenciarConexaoBloqueante (conexao net.Conn){
 			break
 		}
 	}
-<<<<<<< HEAD
-	
-=======
->>>>>>> ec3f6876febe96089581d04c037b52351b173d89
 }
 
 func main(){
